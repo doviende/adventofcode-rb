086ab2f564f370c9cf932e404fe4387da1a6c044
Pete Lypkie <plypkie@itglue.com>
2019-11-26 22:46:53 -0800

    refactor 2_1 tests to use shared examples

--- 2018/spec/p2_1_spec.rb
+++ 2018/spec/p2_1_spec.rb
@@ -1,37 +1,23 @@
 
 require_relative "../p2_1.rb"
+require 'easy_tests'
 
 describe 'letter_counts' do
-  all_tests =
-    {
-      test1: { example: "abcdef", answer: [0, 0] },
-      test2: { example: "bababc", answer: [1, 1] },
-      test3: { example: "aabcdd", answer: [1, 0] }
-    }
-  
-  subject { letter_counts(this_case) }
+  examples = 
+  [
+    { args: ["abcdef"], answer: [0, 0] },
+    { args: ["bababc"], answer: [1, 1] },
+    { args: ["aabcdd"], answer: [1, 0] }
+  ]
 
-  shared_examples "regular tests" do
-    # this_case, answer
-    it "passes" do
-      expect(subject).to eq answer
-    end
-  end
-
-  all_tests.each do |k, v|
-    context "#{v[:example]} --> #{v[:answer]}" do
-      it_behaves_like "regular tests" do
-        let(:this_case) { v[:example] }
-        let(:answer) { v[:answer] }
-      end
-    end
-  end
+  it_behaves_like 'easy test', :letter_counts, examples
 end
 
 describe 'list_checksum' do
-  let(:example) { ["abcdef", "bababc", "aabcdd", "aaabcd", "abbbcd"] }
+  examples = [
+    { args: [["abcdef", "bababc", "aabcdd", "aaabcd", "abbbcd"]], answer: 2*3 },
+  ]
+  
 
-  it 'has the right sum' do
-    expect(list_checksum(example)).to eq 2*3
-  end
+  it_behaves_like 'easy test', :list_checksum, examples
 end
