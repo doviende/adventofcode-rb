4c8ba7e2a9581b820fabe332009a3e5b657533dc
Pete Lypkie <plypkie@itglue.com>
2019-11-28 08:52:43 -0800

    2018 - let easy tests receive a class to send to

--- 2018/spec/p3_1_spec.rb
+++ 2018/spec/p3_1_spec.rb
@@ -8,8 +8,15 @@ describe 'double coverage' do
     "#3 @ 5,5: 2x2"
   ]
   examples = [
-    { args: [tile_list], answer: 4 },
+    { args: [tile_list], answer: [4, 3] },
+  ]
+  parse_examples = [
+    { args: tile_list[0], answer: [1, 1, 3, 4, 4] },
+    { args: tile_list[1], answer: [2, 3, 1, 4, 4] }
   ]
 
-  it_behaves_like 'easy test', :double_covered, examples
+  it_behaves_like 'easy test', :main, examples
+  it_behaves_like 'easy test', :parse_string, parse_examples do
+    let(:klass) { Sector }
+  end
 end
