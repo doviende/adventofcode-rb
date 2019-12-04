4f063100a0d07a1270c4dddc5eae89389238970b
Pete Lypkie <plypkie@itglue.com>
2019-12-03 21:36:55 -0800

    2019 p4 - solve part 2

--- 2019/p4.rb
+++ 2019/p4.rb
@@ -8,13 +8,24 @@
 # How many different passwords within the range given in your puzzle input meet these criteria?
 
 def find_all_passwords(first, last)
-  return (first .. last).select { |x| two_adjacent(x) && digits_increase(x) }.size
+  (first .. last).select { |x| two_adjacent(x) && digits_increase(x) }.size
+end
+
+def find_all_part2_passwords(first, last)
+  (first .. last).select { |x| two_adjacent(x) && digits_increase(x) && two_adjacent_unique(x) }.size
 end
 
 def two_adjacent(x)
   # return true if at least two adjacent digits in x are the same
   same = x.digits(10).each_cons(2).detect { |a,b| a == b }
   !same.nil?
+end
+
+def two_adjacent_unique(x)
+  same = x.digits(10).each_cons(2).select { |a,b| a == b }
+  # have list of doubles that match. need to show that there's at least one unique pair.
+  # convert doubles into singles, and find one that doesn't repeat
+  same.map { |a,b| a }.group_by(&:itself).detect { |k,v| v.size == 1 }
 end
 
 def digits_increase(x)
@@ -28,4 +39,7 @@ if __FILE__ == $0
   input_range = [109165, 576723]
   num = find_all_passwords(input_range[0], input_range[1])
   puts "part 1: #{num} passwords match the criteria in that range"
+
+  num = find_all_part2_passwords(input_range[0], input_range[1])
+  puts "part 2: #{num} passwords match more specific criteria"
 end
