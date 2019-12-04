34b30528bbe77c2afbae55bf928fb67cbe279cd9
Pete Lypkie <plypkie@itglue.com>
2019-12-03 21:06:16 -0800

    2019 p4 - part 1, initial problem statement

---
+++ 2019/p4.rb
@@ -0,0 +1,12 @@
+#/usr/bin/env ruby
+
+input_range = [109165, 576723]
+
+# It is a six-digit number.
+# The value is within the range given in your puzzle input.
+# Two adjacent digits are the same (like 22 in 122345).
+# Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+
+if __FILE__ == $0
+  
+end
