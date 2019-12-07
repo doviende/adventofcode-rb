81304b7cd5d11304c8f5b39fa57451c480028032
Pete Lypkie <plypkie@itglue.com>
2019-12-07 11:29:54 -0800

    2019 p7 - solve part 2

--- 2019/p7.rb
+++ 2019/p7.rb
@@ -2,13 +2,23 @@
 
 require_relative 'intcode'
 
-def run_with_phases(program, phases)
+def run_with_phases(program, phases, feedback=false)
   $stderr.puts("\nrun_with_phases #{phases}\n")
   num_procs = phases.size
-  # purposely one extra here
+  output_stream = -> (x) do
+    if feedback
+      (x+1) % num_procs
+    else
+      x+1
+    end
+  end
+  # make an extra one in case we're not looped:
   iostreams = (0 .. num_procs).map { |x| IO.pipe }
   machines = (0 .. num_procs - 1).map do |x|
-    IntcodeMachine.new(program.dup, iostreams[x][0], iostreams[x+1][1])
+    # if feedback is true, then this will wrap the last output
+    # to be the input of the first one
+    os = output_stream.(x)
+    IntcodeMachine.new(program.dup, iostreams[x][0], iostreams[os][1])
   end
   # seed phases in input
   phases.each_with_index { |p, i| iostreams[i][1].puts(p) }
@@ -18,20 +28,28 @@ def run_with_phases(program, phases)
     $stderr.puts "\nStarting machine #{i}..."
     threads << Thread.new { m.run }
   end
-  threads.each(&:join)
-  answer = iostreams[num_procs][0].gets.chomp.to_i
+  threads.last.join
+  #threads.each(&:join)
+  answer = iostreams[output_stream.(num_procs-1)][0].gets.chomp.to_i
   $stderr.puts "\nlast machine output: #{answer}"
   answer
 end
 
 def run_all_phases(program)
-  answers = (0..4).to_a.permutation(5).to_a.map { |arr| run_with_phases(program, arr) }
+  answers = (0..4).to_a.permutation(5).map { |arr| run_with_phases(program, arr) }
+  answers.max
+end
+
+def run_feedback_phases(program)
+  answers = (5..9).to_a.permutation(5).map { |arr| run_with_phases(program, arr, feedback=true) }
   answers.max
 end
 
 if __FILE__ == $0
   program = DATA.readlines[0].chomp.split(',').map(&:to_i).freeze
   puts "part 1: #{run_all_phases(program)}"
+
+  puts "part 2: #{run_feedback_phases(program)}"
 end
 
 __END__
