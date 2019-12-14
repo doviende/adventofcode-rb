e18e0fce6a68c94e034a3907eb4bac549387f934
Pete Lypkie <plypkie@itglue.com>
2019-12-13 23:35:08 -0800

    2019 p14 - gives wrong answer

--- 2019/p14.rb
+++ 2019/p14.rb
@@ -1,6 +1,198 @@
 #!/usr/bin/env ruby
 
+require 'pry'
+# have ORE, need to produce FUEL. Find out how much ORE you need
+# to produce 1 unit of FUEL.
+
+# plan:
+# - make a stack, push [FUEL, 1]
+# - find a rule that produces fuel,
+# - pop FUEL, push precursors to FUEL
+# - repeat
+# For some ingredient ABC, if we have [ABC, 17] and a rule
+# that says 1 FOO => 11 ABC, then we need to do 17 // 11 = 2
+# and make 22 ABC with 5 extra
+# Then we take the [ABC, 17] out because it's satisfied,
+# and push [FOO, 2] in its place.
+# Because every product (ABC in this case) has a unique rule that
+# produces it, we can always safely pop it and throw away any excess.
+# We can't divide down a rule into smaller bits, but we can always
+# multiply it up to produce more.
+
+# if the amount on the stack is less than required for production by a
+# rule, then we either borrow some from stock to make up the difference,
+# or we multiply the rule.
+
+
+
+class Rule
+  attr_accessor :product, :amount, :reagents
+
+  def initialize(ruletext)
+    # example ruletext: "114 ORE => 4 BHXH"
+    precursors, resultant = ruletext.split(' => ')
+    @product, @amount = to_reagent(resultant).to_a
+    @reagents = to_reagent_list(precursors)
+  end
+
+  def to_reagent(itemtext)
+    amount, name = itemtext.split(' ')
+    Reagent.new(name: name, amount: amount)
+  end
+
+  def to_reagent_list(relist)
+    relist.split(', ').map { |x| to_reagent(x) }
+  end
+
+  def multiply(x)
+    copy = self.dup
+    copy.amount = @amount * x
+    copy.reagents = @reagents.map do |r|
+      rcopy = r.dup
+      rcopy.amount = r.amount * x
+      rcopy
+    end
+    copy
+  end
+end
+
+class Reagent
+  attr_accessor :name, :amount
+
+  def initialize(name:, amount:)
+    @name = name.upcase.to_sym
+    @amount = amount.to_i
+  end
+
+  def to_a
+    [name, amount]
+  end
+end
+
+class FuelCalculator
+  attr_reader :stack, :fuel, :stockpile, :recipes, :later, :ore
+  
+  def initialize(fuel:, recipes:)
+    @fuel = fuel
+    @recipes = recipes
+    @ore = 0
+    @stack = []
+    stack.push(Reagent.new(name: "fuel", amount: @fuel))
+    @stockpile = Hash.new(0)
+    @later = {}
+  end
+  
+  def ore_required
+    loop do
+      process_one
+      merge_common
+      break if stack.size == 0 && later.keys.size == 0
+    end
+    ore
+  end
+
+  def fetch_from_later
+    key = later.keys.first
+    result = Reagent.new(name: key, amount: later[key])
+    later.delete(key)
+    result
+  end
+  
+  def process_one
+    is_later = false
+    if stack.size > 0
+      reagent = stack.pop
+      $stderr.puts "processing #{reagent.to_a} from stack"
+    else
+      # get from later pile.
+      reagent = fetch_from_later
+      $stderr.puts "processing #{reagent.to_a} from later"
+      is_later = true
+    end
+    if reagent.name == :ORE
+      @ore += reagent.amount
+      $stderr.puts "ore = #{ore}"
+      return
+    end
+    rule = recipes[reagent.name]
+    # Before you reduce some reagent to components, you want to make
+    # sure that you've done all the other things that can make that reagent.
+    # This will help you when the rule says " => 10 Foo" but you have 11 Foo,
+    # so you'd normally have to double the rule and increase total requirements.
+    # If you'd waited a bit longer, you'd have realized that you have extra
+    # Foo coming later for free.
+    # On the other hand, it's always safe to do at least one reaction, you
+    # just don't want to accidentally multiply when you don't need to.
+    #
+    # So in our situation here, if reagent.amount is bigger than the amount
+    # made by the rule, then we happily reduce once, and then push the
+    # remainder back onto the stack because it's still a requirement.
+    #
+    # On the other side, if the rule says " => 10 Foo" and we've just popped
+    # 5 Foo off the stack, then we stash it for later. If we run out of things to
+    # reduce on the stack, then we go into the "later" pile and do one of them, which
+    # may involve producing extra into the stockpile.
+
+    # add required "later" amount if available
+    needed = later[reagent.name]
+    if !needed.nil? && needed > 0
+      reagent.amount += needed
+      later.delete(reagent.name)
+    end
+
+    # if a requirement popped off the stack and we have some of that already
+    # in the stockpile, then we just subtract the stockpile amount and keep going.
+    extra = stockpile[reagent.name]
+    if extra > 0
+      if extra < reagent.amount
+        reagent.amount -= extra
+        stockpile[reagent.name] = 0
+      else
+        # extra >= amount
+        diff = extra - reagent.amount
+        stockpile[reagent.name] = diff
+        return  # work complete
+      end
+    end
+
+    if reagent.amount >= rule.amount || is_later
+      # --> do the rule as many times as we can
+      # - find the multiplier
+      multiplier = [reagent.amount / rule.amount, 1].max
+      # - multiply all the rule.reagents by the multiplier
+      #   and push them on the stack.
+      newrule = rule.multiply(multiplier)
+      results = newrule.reagents
+      results.each { |r| stack.push(r) }
+      # - subtract the multiplied rule amount from reagent.amount
+      if reagent.amount < newrule.amount
+        stockpile[reagent.name] += newrule.amount - reagent.amount
+      else
+        reagent.amount -= newrule.amount
+      end
+    end
+    if reagent.amount > 0
+      later[reagent.name] = reagent.amount
+    end
+  end
+
+  def merge_common
+    # if any elements of the stack are the same name, then add them up
+    tmp = stack.reduce(Hash.new(0)) do |hash, reagent|
+      hash[reagent.name] += reagent.amount
+      hash
+    end
+    stack = tmp.to_a
+  end
+end
+
 if __FILE__ == $0
+  rules = DATA.readlines(chomp: true).map { |ruletext| Rule.new(ruletext) }
+  recipes = rules.reduce({}) do |sum, rule|
+    sum.merge({ rule.product => rule })
+  end
+  ore = FuelCalculator.new(fuel: 1, recipes: recipes).ore_required
+  puts "part 1: ore = #{ore}"
 end
 
 __END__
