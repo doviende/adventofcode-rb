1a4addd4eb015df5a613cc0c3cd56f84f9ca227c
Pete Lypkie <plypkie@itglue.com>
2020-10-25 17:19:48 -0700

    add instance tester to easy_tests

--- 2018/spec/easy_tests.rb
+++ 2018/spec/easy_tests.rb
@@ -14,3 +14,27 @@ shared_examples "easy test" do |function, example_array|
     end
   end
 end
+
+
+shared_examples "easy test instance" do |function, example_array|
+  let(:klass) { nil }
+  # example array has possible keys of:
+  #   init: args for creating an instance
+  #   args: args for calling function on the instance
+  #   answer: the expected result of that call
+
+  context "#{function}" do
+    example_array.each do |dict|
+      context ".new(#{Array(dict[:init]).join(", ")}).#{function}(#{Array(dict[:args]).join(", ")})" do
+        let(:example_args) { Array(dict[:args]) }
+        let(:init_args) { Array(dict[:init]) }
+        let(:answer) { dict[:answer] }
+        let(:instance) { klass.new(*init_args) }
+        subject { instance.send(function, *example_args) }
+        it "equals #{dict[:answer]}" do
+          expect(subject).to eq answer
+        end
+      end
+    end
+  end
+end
