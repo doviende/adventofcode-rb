fbcca2e4d7c918ce8970d1d36076e443470c9c48
Pete Lypkie <plypkie@itglue.com>
2020-12-12 21:01:17 -0800

    2020 p13 - empty file and input

---
+++ 2020/p13.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require "active_support/core_ext/object/blank"
+
+if __FILE__ == $0
+  lines = DATA.readlines(chomp: true)
+
+end
+
+__END__
+1008832
+23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,449,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,991,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17
