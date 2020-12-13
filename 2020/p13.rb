03a872e0ca3464609ad3a925df66ae6b97692f9c
Pete Lypkie <plypkie@itglue.com>
2020-12-12 23:46:32 -0800

    2020 p13 - add puts to help illustrate, not done.

--- 2020/p13.rb
+++ 2020/p13.rb
@@ -9,6 +9,8 @@ class MultFinder
       next if n == "x"
       @offsets[n.to_i] = i
     end
+    @base = @offset_input[0].to_i
+    puts "input: #{offset_input}"
   end
 
   def remainder_for(n)
@@ -16,23 +18,25 @@ class MultFinder
   end
 
   def find_multiplier_for(n)
-    # multiply 23 by successive ints mod n until we hit remainder_for(n)
+    # multiply @base by successive ints mod n until we hit remainder_for(n)
     puts "finding multiplier for #{n}:"
     multiplier = nil
     (1..).each do |m|
-
       multiplier = m
-      remainder = (23*m) % n
+      remainder = (@base * m) % n
       puts "#{n} with mult #{m}: remainder is #{remainder}, searching for #{remainder_for(n)}"
-      break if (23*m) % n == remainder_for(n)
+      break if (@base * m) % n == remainder_for(n)
     end
     multiplier
   end
 
   def find_number
     mults = @offsets.keys.map { |k| find_multiplier_for(k) }
-    answer = mults.reduce(:*)
+    answer = @base * mults.reduce(:*)
     puts "mults: #{mults} --> #{answer}"
+    @offsets.keys.each do |k|
+      puts "#{k} - #{answer} % #{k} = #{k - (answer % k)}"
+    end
     answer
   end
 end
@@ -61,6 +65,21 @@ if __FILE__ == $0
   # 28 more than a multiple of 41. so if we're multiplying 23 by something in mod 41 until we get to 28. So let's
   # say we find a number f1 such that 23*f1 = 28 (mod 41). i think we now know that t is divisible by 23*f1.
   # now we do the same for 449, where we want 23*f2 = 426 (mod 449), and then t is divisible by 23*f1*f2
+  #
+  # Doh! ok, that didn't quite work. the problem is that when 23*f1 hits 28 mod 41, once you multiply it by f2,
+  # it no longer does. so f2 can't be a simple multiplier, it has to be something that is also invisible to mod 41,
+  # ie it has to be 41, 82, 123, etc. And then when you go to f3, it has to be m*41*449. Maybe f1 has to be a multiple of
+  # the LCM of the other IDs?
+  #
+  # example problem: "17, x, 13, 19".  need 17*f1*f2 such that 17*(m1*19) = 11 mod 13, and 17*(m2*13) = 16 mod 19,
+  # If 17*f1 = 11 mod 13, there are several f1s that fit this. We need one that also matches the other conditions
+  # like 17*f1 = 16 mod 19. If we start with the least f1 that satisfies, we then want to make it bigger somehow while
+  # still preserving its value mod 13...so we can ...add 13 to it? So f1=6 is the lowest number such that 17*f1 = 11 mod 13.
+  # so what about 13 + 6 = 19? 17*19 = 11 mod 13 as well, but it is 0 mod 19. try again. 19+13 = 32. keep adding 13 until
+  # it works for 19 as well. so 6 + 13n = 11 + 19m. in this case, n = 15 and m = 10
+  #
+  # What do we do when there are more numbers? it was easy to just add 13 a couple times and get to the right value mod 19,
+  # but then what's the next one? i guess we add LCM(13,19) over and over until it matches.
 
   # steps
   # 1) find the offset of each ID number.
